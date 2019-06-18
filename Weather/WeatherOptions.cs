b64884c3a105a312e597b552a2837f95049c5861
ElPlatero <elplatero@baelgun.de>
2019-06-18 13:27:44 +0200

    switched api for current weather from yahoo to owm

--- Weather/WeatherOptions.cs
+++ Weather/WeatherOptions.cs
@@ -3,7 +3,7 @@
     public class WeatherOptions
     {
         public int CityId { get; set; } = 2929670;
-        public string CityName { get; set; } = "Erfurt, Germany";
+        public string OwmKey { get; set; } = "Default";
         public int RefreshInterval { get; set; } = 120;
     }
 }
