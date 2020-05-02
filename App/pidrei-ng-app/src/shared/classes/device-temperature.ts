79b9cd153e1dcb9d32e8790f679abaf8dc5dc181
Tino <elplatero@baelgun.de>
2020-05-02 11:03:23 +0200

    Elplatero/migrations for netcore 31 (#24)
    * migrated to 3.1
    
    * fixed some 2.2->3.1 migration issues

--- App/pidrei-ng-app/src/shared/classes/device-temperature.ts
+++ App/pidrei-ng-app/src/shared/classes/device-temperature.ts
@@ -2,6 +2,11 @@ export class DeviceTemperature {
     public temperature: number;
     public offset: number;
 
+    constructor(initialValue: { temperature?: number, offset?: number } = { temperature: 0, offset: 0 }) {
+        this.temperature = initialValue.temperature;
+        this.offset = initialValue.offset;
+    }
+
     public getTemperature(): number {
         return this.temperature + this.offset;
     }
